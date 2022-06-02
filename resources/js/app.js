018db68056f88fae7950f2b12968cd8fc911e228
Arslan <3103arsl@gmail.com>
2022-06-02 22:38:18 +0500

    //Vue Installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue'
+import Board from './components/Board'
+
+const app = createApp({})
+
+app.component('task-board', Board)
+
+app.mount('#app')
